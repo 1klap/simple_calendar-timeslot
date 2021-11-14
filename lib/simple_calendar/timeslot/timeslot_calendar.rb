28a4548492b761373641ae995b6bf9933b2c3845
Kim Laplume <kim.laplume@protonmail.com>
2021-11-14 21:08:21 +0100

    add support for attribute and end_attribute options, and clean code up

--- lib/simple_calendar/timeslot/timeslot_calendar.rb
+++ lib/simple_calendar/timeslot/timeslot_calendar.rb
@@ -1,53 +1,31 @@
 require "simple_calendar"
 
 module SimpleCalendar
-  module Timeslot
+  module Timeslot    
     class TimeslotCalendar < SimpleCalendar::Calendar
       MINUTE_HEIGHT_PX = 0.65
       FIRST_HOUR_SLOT = 0
-    
-      def height
-        h = (24 - TimeslotCalendar::FIRST_HOUR_SLOT) * 60 * px_per_minute
-        h = h+bucket_title_size if display_bucket_title
-        h
-      end
-    
-      def event_height(event, day)
-        minutes = if event.start_time.to_date != day
-                    (event.end_time - event.end_time.midnight)/60
-                  elsif event.start_time.to_date < event.end_time.to_date
-                    (event.end_time.midnight - 60 - event.start_time)/60
-                  else
-                    event.duration
-                  end
-        minutes * px_per_minute
-      end
-    
-      def event_top_distance(event, day)
-        return 0 if event.start_time.to_date != day
-        (event.start_time.hour - TimeslotCalendar::FIRST_HOUR_SLOT) * 60 * px_per_minute + event.start_time.min * px_per_minute
-      end
-    
+
       def orientation
         @options.fetch(:orientation, :vertical)
       end
-    
+
       def horizontal_height_px
         @options.fetch(:horizontal_height_px, 300)
       end
-    
+
       def split_by_type
         @options.fetch(:split_by_type, false)
       end
-    
+
       def px_per_minute
         @options.fetch(:px_per_minute, TimeslotCalendar::MINUTE_HEIGHT_PX)
       end
-    
+
       def display_bucket_title
         @options.fetch(:display_bucket_title, false)
       end
-    
+
       def bucket_title_size
         if display_bucket_title
           @options.fetch(:bucket_title_size, 20)
@@ -55,11 +33,11 @@ module SimpleCalendar
           0
         end
       end
-    
+
       def grid_top_offset(hour)
         4.16666667 * hour
       end
-    
+
       def grid_width
         if display_grid
           @options.fetch(:grid_width, "20px")
@@ -67,11 +45,35 @@ module SimpleCalendar
           0
         end
       end
-    
+
       def display_grid
         @options.fetch(:display_grid, true)
       end
-    
+
+      def height
+        #h = (24 - TimeslotCalendar::FIRST_HOUR_SLOT) * 60 * px_per_minute
+        h = 24 * 60 * px_per_minute
+        h = h+bucket_title_size if display_bucket_title
+        h
+      end
+
+      def event_height(event, day)
+        minutes = if event.send(attribute).to_date != day
+                    (event.send(end_attribute) - event.send(end_attribute).midnight)/60
+                  elsif event.send(attribute).to_date < event.send(end_attribute).to_date
+                    (event.send(end_attribute).midnight - 60 - event.send(attribute))/60
+                  else
+                    event.duration
+                  end
+        minutes * px_per_minute
+      end
+
+      def event_top_distance(event, day)
+        return 0 if event.send(attribute).to_date != day
+        #(event.send(attribute).hour - TimeslotCalendar::FIRST_HOUR_SLOT) * 60 * px_per_minute + event.send(attribute).min * px_per_minute
+        event.send(attribute).hour * 60 * px_per_minute + event.send(attribute).min * px_per_minute
+      end
+
       def split_into_buckets(events)
         if split_by_type
           events.group_by{|e| e.send split_by_type}.values
@@ -79,33 +81,34 @@ module SimpleCalendar
           [events]
         end
       end
-    
+
       def slot_events(events, day)
         r = {}
         events.each do |event|
           r[event] = [0, 0, event_height(event, day), event_top_distance(event, day)]
         end
-        #
+        # Credit: https://stackoverflow.com/questions/11311410/visualization-of-calendar-events-algorithm-to-layout-events-with-maximum-width
+        # Author: Markus Jarderot (https://stackoverflow.com/users/22364/markus-jarderot)
         columns = [[]]
         last_event_ending = nil
         events.each do |event|
-          if(!last_event_ending.nil? && event.start_time > last_event_ending)
+          if !last_event_ending.nil? && event.send(attribute) > last_event_ending
             pack_events(r, columns)
-            colums = [[]]
+            columns = [[]]
             last_event_ending = nil
           end
           placed = false
           columns.each do |col|
-            if !events_collide(r, col.last, event)
+            unless events_collide(r, col.last, event)
               col << event
               placed = true
               break
             end
           end
-          if !placed
+          unless placed
             columns << [event]
           end
-          event_end_time = event.start_time + event.duration.minutes
+          event_end_time = event.send(end_attribute)
           if last_event_ending.nil? || event_end_time > last_event_ending
             last_event_ending = event_end_time
           end
@@ -115,7 +118,9 @@ module SimpleCalendar
         end
         r
       end
-    
+
+      private
+
       def pack_events(r, columns)
         num_columns = columns.size.to_f
         columns.each_with_index do |col, iter_col|
@@ -126,14 +131,14 @@ module SimpleCalendar
           end
         end
       end
-    
+
       def events_collide(r, event1, event2)
         return false if event1.nil? || event2.nil?
         event1_bottom = r[event1][3] + r[event1][2]
         event2_bottom = r[event2][3] + r[event2][2]
         event1_bottom > r[event2][3] && r[event1][3] < event2_bottom
       end
-    
+
       def expand_event(r, event, iter_col, columns)
         col_span = 1
         columns.each_with_index do |column, index|
@@ -146,8 +151,6 @@ module SimpleCalendar
         end
         col_span
       end
-    
-      private
     end
   end
 end
